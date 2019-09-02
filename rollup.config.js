299226662f8617b4d17caf6c7b6b24b82ee85393
Richard Worsfold <rwwors@gmail.com>
2019-09-01 23:02:07 -0700

    Fix: Config

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,26 @@
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
 
 export default [
   {
     input: 'src/main.ts',
-    output: {
-      name: 'getBoundingClientRectsAsync',
-      file: 'dist/bundle.js',
-      format: 'umd',
-    },
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      },
+      {
+        file: pkg.browser,
+        format: 'iife',
+        name: 'AsyncBounds',
+      },
+    ],
     plugins: [resolve(), commonjs(), typescript()],
   },
 ];
